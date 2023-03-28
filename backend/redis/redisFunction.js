9bf02bab825031b1a8968795b4e72c46dd1dcba5
Arjun E <arjunrk907@gmail.com>
2023-03-28 16:21:05 +0530

    fix redis error handling

--- backend/redis/redisFunction.js
+++ backend/redis/redisFunction.js
@@ -2,32 +2,44 @@ import { connectToRedis } from "./connect.js";
 
 // function for get or set data from redis
 export async function getOrSetFunction(key, cb) {
-  const redisClient = await connectToRedis();
-  const res = await redisClient.get(key);
-  if (res != null) {
-    console.log("from redis");
-    return JSON.parse(res);
+  try {
+    const redisClient = await connectToRedis();
+    const res = await redisClient.get(key);
+    if (res != null) {
+      console.log("from redis");
+      return JSON.parse(res);
+    }
+    const fetchData = await cb();
+    redisClient.setEx(key, process.env.REDIS_EXPIRE, JSON.stringify(fetchData));
+    return fetchData;
+  } catch (err) {
+    return err;
   }
-  const fetchData = await cb();
-  redisClient.setEx(key, process.env.REDIS_EXPIRE, JSON.stringify(fetchData));
-  return fetchData;
 }
 
 // update data
 export async function updateCacheMemory(key, data) {
-  const redisClient = await connectToRedis();
-  const res = await redisClient.setEx(
-    key,
-    process.env.REDIS_EXPIRE,
-    JSON.stringify(data)
-  );
-  return res;
+  try {
+    const redisClient = await connectToRedis();
+    const res = await redisClient.setEx(
+      key,
+      process.env.REDIS_EXPIRE,
+      JSON.stringify(data)
+    );
+    return res;
+  } catch (error) {
+    return error;
+  }
 }
 
 export async function deleteCache(key) {
-  const redisClient = await connectToRedis();
-  const del = await redisClient.del(key);
-  return del;
+  try {
+    const redisClient = await connectToRedis();
+    const del = await redisClient.del(key);
+    return del;
+  } catch (error) {
+    return error;
+  }
 }
 
 /**
@@ -36,7 +48,11 @@ export async function deleteCache(key) {
  * @returns {object} - payment order data from redis
  */
 export async function getCacheData(key) {
-  const redisClient = await connectToRedis();
-  const data = await redisClient.get(key);
-  return JSON.parse(data);
+  try {
+    const redisClient = await connectToRedis();
+    const data = await redisClient.get(key);
+    return JSON.parse(data);
+  } catch (error) {
+    return error;
+  }
 }
